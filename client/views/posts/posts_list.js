20893c9f9d4c71f5addc6724c9b4a1548d00ff9d
Evan Turner <evturn@gmail.com>
2015-02-07 18:50:07 -0500

    post data added

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+	{
+		title: 'Introducing Telescope',
+		author: 'Michael Jordan',
+		url: 'http://nba.com'
+	},
+	{
+		title: 'Meteor',
+		author: 'Penny Hardaway',
+		url: 'http://craigslist.org'
+	},
+	{
+		title: 'Dudeman\'s Guide to Dudes',
+		author: 'Dudeman',
+		url: 'http://evturn.com'
+	}
+];
+Template.postList.helpers({
+	posts:postsData
+});
